ed30ae024c78b700ec760e98e990d62fcd3b92a2
Rick <56639624@qq.com>
2018-07-23 21:23:57 -0400

    update models info

--- www/models.py
+++ www/models.py
@@ -29,3 +29,21 @@ class Blog(Model):
 	id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
 	user_id = StringField(ddl='varchar(50)')
 	user_name = StringField(ddl='varchar(50)')
+	user_image = StringField(ddl='varchar(500)')
+	name = StringField(ddl='varchar(50)')
+	summary = StringField(ddl='varchar(200)')
+	content = TextField()
+	created_at = FloatField(default=time.time)
+
+
+class Comment(Model):
+	__table__ = 'comments'
+
+	id = StringFiled(primary_key=True, default=next_id, ddl='varchar(50)')
+	blog_id = StringField(ddl='varchar(50)')
+	user_id = StringField(ddl='varchar(50)')
+	user_name = StringField(ddl='varchar(50)')
+	user_image = StringField(ddl='varchar(500)')
+	content = TextField()
+	created_at = FloatField(default=time.time)
+
