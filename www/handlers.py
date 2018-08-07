5c144085f5ebe1edcb025f72e96d9df09716a181
Rick <56639624@qq.com>
2018-08-07 05:28:57 -0400

    add blogs test for web

--- www/handlers.py
+++ www/handlers.py
@@ -19,9 +19,14 @@ from models import User, Comment, Blog, next_id
 
 @get('/')
 async def index(request):
-	users = await User.findAll()
-	logging.info('users size: ', len(users))
+	summary = 'tree命令可以以树形结构显示文件目录结构，它非常适合于我们给别人介绍我们的文件目录的组成框架，同时该命令使用适当的参数也可以将命令结果输出到文本文件中。'	
+	blogs = [
+		Blog(id='1', name='编码', summary=summary, created_at=time.time()-120),
+		Blog(id='1', name='读书', summary=summary, created_at=time.time()-360),
+		Blog(id='1', name='机器学习', summary=summary, created_at=time.time()-720),
+		Blog(id='1', name='Deep learning', summary=summary, created_at=time.time()-1020)
+	]
 	return {
-		'__template__': 'test.html',
-		'users': users
+		'__template__': 'blogs.html',
+		'blogs': blogs
 	}
