bcf08b57c4deecca245e121c1de9384e6a107bc9
Rick <56639624@qq.com>
2018-08-07 22:33:06 +0800

    add register view

--- www/handlers.py
+++ www/handlers.py
@@ -22,11 +22,19 @@ async def index(request):
 	summary = 'tree命令可以以树形结构显示文件目录结构，它非常适合于我们给别人介绍我们的文件目录的组成框架，同时该命令使用适当的参数也可以将命令结果输出到文本文件中。'	
 	blogs = [
 		Blog(id='1', name='编码', summary=summary, created_at=time.time()-120),
-		Blog(id='1', name='读书', summary=summary, created_at=time.time()-360),
-		Blog(id='1', name='机器学习', summary=summary, created_at=time.time()-720),
-		Blog(id='1', name='Deep learning', summary=summary, created_at=time.time()-1020)
+		Blog(id='2', name='读书', summary=summary, created_at=time.time()-360),
+		Blog(id='3', name='机器学习', summary=summary, created_at=time.time()-720),
+		Blog(id='4', name='Deep learning', summary=summary, created_at=time.time()-1020)
 	]
 	return {
 		'__template__': 'blogs.html',
 		'blogs': blogs
 	}
+
+
+@get('/api/users')
+async def api_get_users():
+	users = await User.findAll(orderBy='created_at desc')
+	for u in users:
+		u.passwd = '******'
+	return dict(users=users)
